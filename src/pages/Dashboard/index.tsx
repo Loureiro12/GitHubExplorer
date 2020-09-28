8277ed2e8e5235ae3adaab9eb9196e7b2762edd5
Andre Loureiro <contato.loureiro1@gmail.com>
2020-09-28 11:40:19 -0300

    Conectando a API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,74 +1,66 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi'
+import api from '../../services/api';
 
 import logo from '../../assets/imagem_2.svg';
 import Repository from '../Repository';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
+}
+
 const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+}
+
     return (
         <>
             <img src={logo} alt="GitHub Explorer" />
             <Title>Explore repositório no GitHub</Title>
 
-            <Form>
-                <input placeholder="Digite o nome de repositorio" />
+            <Form onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder="Digite o nome de repositorio"
+                />
                 <button type="submit">Pesquisar</button>
             </Form>
 
             <Repositories>
-                <a href="teste">
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="teste">
                     <img
-                        src="https://avatars0.githubusercontent.com/u/65689062?s=460&u=f5fa736dcab08e9f129bacb749d590c9187daaa1&v=4"
-                        alt="André Loureiro"
+                        src={repository.owner.avatar_url}
+                        alt={repository.owner.login}
                     />
                     <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>Easy peasy highly scalable ReactJs</p>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
                     </div>
 
                     <FiChevronRight size={20} />
                 </a>
+                ))}
 
-                <a href="teste">
-                    <img
-                        src="https://avatars0.githubusercontent.com/u/65689062?s=460&u=f5fa736dcab08e9f129bacb749d590c9187daaa1&v=4"
-                        alt="André Loureiro"
-                    />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>Easy peasy highly scalable ReactJs</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
-
-                <a href="teste">
-                    <img
-                        src="https://avatars0.githubusercontent.com/u/65689062?s=460&u=f5fa736dcab08e9f129bacb749d590c9187daaa1&v=4"
-                        alt="André Loureiro"
-                    />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>Easy peasy highly scalable ReactJs</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
-
-                <a href="teste">
-                    <img
-                        src="https://avatars0.githubusercontent.com/u/65689062?s=460&u=f5fa736dcab08e9f129bacb749d590c9187daaa1&v=4"
-                        alt="André Loureiro"
-                    />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>Easy peasy highly scalable ReactJs</p>
-                    </div>
-
-                    <FiChevronRight size={20} />
-                </a>
             </Repositories>
         </>
     );
