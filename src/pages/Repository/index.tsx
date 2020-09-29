bdc03de3ae7a451cfe98a07763efae3607ada99a
Andre Loureiro <contato.loureiro1@gmail.com>
2020-09-29 11:56:38 -0300

    Estilizando detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+
+import logo from '../../assets/imagem_2.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
     repository: string;
@@ -8,7 +13,53 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository:{params.repository}</h1>
+    return (
+        <>
+            <Header>
+                <img src={logo} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronsLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img src="https://lh3.googleusercontent.com/ogw/ADGmqu-Cf4sx2swvjcXgPs-cOfb2hhP9SehTqUgSLgYv=s64-c-mo" alt="Teste" />
+                    <div>
+                        <strong>Rocketseat/unform</strong>
+                        <p>descrição do repositorio</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1808</strong>
+                        <span>Start</span>
+                    </li>
+
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+
+                <Issues>
+                    <Link to="dasda">
+                        <div>
+                            <strong>fdafa</strong>
+                            <p>fafdasdf</p>
+                        </div>
+
+                        <FiChevronRight size={20} />
+                    </Link>
+                </Issues>
+            </RepositoryInfo>
+        </>
+    );
 }
 
 export default Repository;
